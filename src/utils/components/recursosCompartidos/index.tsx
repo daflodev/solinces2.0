85e6818f70f28c9c3fdc997b4c6e17e7b62c8a19
Randy Salas Herrera <salasrandy89@gmail.com>
2023-07-27 00:32:32 -0500

    feat: upload de archivo y listado de archivo y url

--- src/utils/components/recursosCompartidos/index.tsx
+++ src/utils/components/recursosCompartidos/index.tsx
@@ -1,13 +1,16 @@
-import { Col, Input, Row, Table } from "antd";
-import { LinkOutlined } from '@ant-design/icons';
-import { useEffect, useState } from "react";
+import { Button, Col, Input, Row, Table, message } from "antd";
+import { LinkOutlined, CloudUploadOutlined } from '@ant-design/icons';
+import { useEffect, useRef, useState } from "react";
 import { calificationStore } from "@/store/calificationStore";
 import {shallow} from "zustand/shallow";
 import { QueryBuilders } from "@/utils/orm/queryBuilders";
+import { sessionInformationStore } from "@/store/userInformationStore";
+import { ApiServicesMembrete } from "@/utils/services/api/services";
 
 export const RecursosCompartidos = () => {
 
   const currentDate = new Date();
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
    // @ts-ignore
    const { currentAsignature,currentGrade,currentGroup, currentEvaluationPeriod } =
@@ -21,8 +24,15 @@ export const RecursosCompartidos = () => {
        shallow
    );
 
+   const { currentCampus } = sessionInformationStore(
+    (state) => ({
+        currentCampus: state.currentCampus
+    }),
+    shallow
+);
+
   const [data, setData] = useState<any[]>([])  
-  let inputUrl = ''
+  let inputUrl: any
   let inputDescripcion = ''
       
   const columns = [
@@ -51,10 +61,85 @@ export const RecursosCompartidos = () => {
     inputDescripcion = e.target.value;
   }
 
-  const convertirFecha = (fechaHoraISO: string): string => {
-    const fecha = new Date(fechaHoraISO);
-    const fechaFormateada = fecha.toISOString().split('T')[0];
-    return fechaFormateada;
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+    fileInputRef.current.click();
+    }
+  };
+
+  const onChange = (event: any) => {
+    console.log(event.target.files[0], 'archivo')
+    inputUrl = event.target.files[0]
+  };
+
+  // const convertirFecha = (fechaHoraISO: string): string => {
+  //   const fecha = new Date(fechaHoraISO);
+  //   const fechaFormateada = fecha.toISOString().split('T')[0];
+  //   return fechaFormateada;
+  // }
+
+  const saveData = async () => {
+
+    const data: object = {
+        file: inputUrl,
+        descripcion: inputDescripcion,
+        idsede: currentCampus?.value,
+        etiqueta: 'recurso'
+    }
+
+    message.loading('cargando...')
+
+    await ApiServicesMembrete(data)
+        .then((response) => {
+            // setImageUrl(response.data.data.URLS3)
+            console.log(response.data.response.url, '---')
+            saveArchive(response.data.response.nombre,'', response.data.response.id)
+            // message.destroy()
+            // saveArchive(response.data.data.nombre,response.data.data.URLS3)
+            // message.success('enviado')
+          
+        })
+        .catch(() => {
+            message.success('error')
+        });
+
+  }
+
+  const saveArchive = (name: string, url: any, id?: any) => {
+    const querycolumn = new QueryBuilders('recurso_compartido');
+    if(id){
+      querycolumn
+      .create({
+        'fk_tasignatura': 3007,
+        'fk_tgrupo': 3486,
+        'fecha_creacion': formatearFecha(currentDate),
+        'fecha_publicacion': formatearFecha(currentDate),
+        'nombre': name,
+        'descripcion': inputDescripcion,
+        'fk_tarchivo': id
+      })
+      .schema('ACADEMICO_COL0')
+      .save()
+      getData()
+
+    }else{
+      querycolumn
+      .create({
+        'fk_tasignatura': 3007,
+        'fk_tgrupo': 3486,
+        'fecha_creacion': formatearFecha(currentDate),
+        'fecha_publicacion': formatearFecha(currentDate),
+        'nombre': name,
+        'descripcion': inputDescripcion,
+        'url_recurso_externo': url,
+      })
+      .schema('ACADEMICO_COL0')
+      .save()
+      getData()
+
+    }
+
+    
   }
 
   const formatearFecha = (fecha: Date): string =>{
@@ -67,33 +152,24 @@ export const RecursosCompartidos = () => {
   const getData = async () => {
     const querycolumn = new QueryBuilders('recurso_compartido');
     const getdata: any = await querycolumn
-                    .select(['"PK_TRECURSO_COMPARTIDO" AS id' ,'"NOMBRE" AS name','"DESCRIPCION" as description','"URL_RECURSO_EXTERNO" AS url', '"FECHA_CREACION" AS date'])
+                    .select(['"PK_TRECURSO_COMPARTIDO" AS id' ,
+                            '"NOMBRE" AS name',
+                            '"DESCRIPCION" as description',
+                            'CASE WHEN "URL_RECURSO_EXTERNO" IS NOT NULL THEN "URL_RECURSO_EXTERNO" ELSE (SELECT "URLS3" FROM "ACADEMICO_COL0"."TARCHIVO" WHERE "PK_TARCHIVO" = "FK_TARCHIVO") END AS url',
+                            '"FECHA_CREACION" AS date'])
                     .schema('ACADEMICO_COL0')
                     .orderBy("id", "desc")
-                    .save()
+                    .get()
     setData(getdata)
   }
 
   const createUrl = async () => {
-    const querycolumn = new QueryBuilders('recurso_compartido');
-    await querycolumn
-                    .create({
-                      'fk_tasignatura': 3007,
-                      'fk_tgrupo': 3486,
-                      'fecha_creacion': formatearFecha(currentDate),
-                      'fecha_publicacion': formatearFecha(currentDate),
-                      'nombre': inputUrl.replace('http://','').replace('https://',''),
-                      'descripcion': inputDescripcion,
-                      'url_recurso_externo': inputUrl
-                    })
-                    .schema('ACADEMICO_COL0')
-                    .save()
-    getData()
+    saveArchive(inputUrl, inputUrl)
   }
 
   const addColum = () =>{
 
-    const column = {
+    const column : any = {
       name: (
         <Row justify="start">
          <Col span={3}> <LinkOutlined style={{ marginTop: '10px' }} /> </Col> 
@@ -106,10 +182,29 @@ export const RecursosCompartidos = () => {
         </>
       ),
       date: currentDate.toLocaleDateString(),
-      type: 'pdf'
     }
     setData([column, ...data]);
   }
+
+  const addColumUpload = () =>{
+
+    const column : any = {
+      name: (
+        <Row justify="center">
+          <input style={{ display: 'none' }} ref={fileInputRef} type="file" onChange={onChange} />
+          <Button onClick={handleButtonClick} type="primary" shape="round" icon={<CloudUploadOutlined />} size={'large'}/> 
+        </Row>
+      ),
+      description: (
+        <>
+          <Input onChange={onChangeDescription} onPressEnter={saveData} placeholder="Descripcion" />
+        </>
+      ),
+      date: currentDate.toLocaleDateString(),
+    }
+    setData([column, ...data]);
+  }
+  
 
   useEffect(() => {
     getData()
@@ -123,7 +218,14 @@ export const RecursosCompartidos = () => {
         <br/>
         {/* {currentAsignature} - {currentGrade} - {currentGroup} - {currentEvaluationPeriod} */}
         <br/>
-         <div onClick={addColum} style={{ cursor:'pointer', width: '20px' }} ><LinkOutlined /></div>
+        <Row gutter={2}>
+          <Col span={1}>
+            <div onClick={addColumUpload} style={{ cursor:'pointer', width: '20px' }} ><CloudUploadOutlined /></div>
+          </Col>
+          <Col span={1}>
+            <div onClick={addColum} style={{ cursor:'pointer', width: '20px' }} ><LinkOutlined /></div> 
+          </Col>
+        </Row>
         <Table 
           dataSource={data}
           columns={columns}
